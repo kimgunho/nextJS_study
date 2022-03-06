16d25b0ad79740b81896336a19c7dd373302bb57
kim <yuyukgh@naver.com>
2022-03-06 23:59:39 +0900

    Feat : 임시 브랜드 페이지 생성

--- pages/brand.js
+++ pages/brand.js
@@ -1,3 +1,31 @@
+import Seo from "../components/Seo";
+
 export default function Brand() {
-  return <div>Brand</div>;
+  return (
+    <>
+      <div className="wrapper">
+        <Seo title={"brand"} />
+        <p>
+          Sint enim exercitation aute consequat ea ad aute sit esse duis eu
+          culpa.
+        </p>
+      </div>
+
+      <style jsx>{`
+        .wrapper {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 50vh;
+        }
+        p {
+          text-align: center;
+          font-size: 1rem;
+          line-height: 2;
+          font-size: 1.125rem;
+          font-weight: lighter;
+        }
+      `}</style>
+    </>
+  );
 }
